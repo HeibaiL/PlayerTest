b34309d6b72ddefe78ad657a265aff24b27f33ff
unknown <vladikd96@gmail.com>
2020-03-27 09:36:38 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Video from "./components/Video";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Video/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Video.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Video = () =>{
+    return (<div className="video-block">
+   <iframe src="http://www.youtube.com/embed/W7qWa52k-nE"
+   width="560" height="315" frameBorder="0" allowFullScreen={true} ></iframe>
+    </div>)
+};
+export default Video;
